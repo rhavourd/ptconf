3c523b191a70feb6a03d792c1d4d83a14f1704ad
Rick Havourd <rhavourd@schools-open.com>
2012-07-26 09:00:47 -0400

    Conflict resolved

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -11,13 +11,10 @@
 
 class Teacher < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
-=======
   has_many :courses, :dependent => :destroy
   has_many :conference_dates, :dependent => :destroy
 
   def full_name
     user.full_name  if user.present?
   end
->>>>>>> jimjones
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,11 @@
 # It's strongly recommended to check this file into your version control system.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20120713160517) do
+=======
+ActiveRecord::Schema.define(:version => 20120725184612) do
+>>>>>>> jimjones
 =======
 ActiveRecord::Schema.define(:version => 20120725184612) do
 >>>>>>> jimjones
